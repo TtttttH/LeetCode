980bc148df85280e3ccf3ccf873a7eccdfadc555
TtttttH <574173437@qq.com>
2022-01-08 09:01:38 +0000

    No.700 二叉搜索树中的搜索(easy)

---
+++ src/middle/binarytree/SearchBST_700.java
@@ -0,0 +1,14 @@
+package middle.binarytree;
+
+/**
+ * No.700 二叉搜索树中的搜索
+ */
+public class SearchBST_700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+
+        return root.val < val? searchBST(root.right, val) : searchBST(root.left, val);
+    }
+}
