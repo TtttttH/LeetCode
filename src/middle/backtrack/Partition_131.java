b81bd8809b3878d8de524830c89839c1b0b335cc
TtttttH <574173437@qq.com>
2022-01-12 19:50:21 +0000

    回溯法 No.131 回文串分割 需要二刷 hard 难度

--- src/middle/backtrack/Partition_131.java
+++ src/middle/backtrack/Partition_131.java
@@ -1,23 +1,47 @@
 package middle.backtrack;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- * No.131 分割回文串
+ * No.131 分割回文串 （难度可以算hard)
  */
 public class Partition_131 {
 
     List<List<String>> results = new ArrayList<>();
-
+    LinkedList<String> path = new LinkedList<>();
     public List<List<String>> partition(String s) {
         if (s == null || s.length() == 0) {
             return results;
         }
 
-
-        results;
+        backtracking(s, 0);
+        return results;
     }
 
-    private void backtracking(String s,)
+    private void backtracking(String s, int startIndex) {
+        if (startIndex >= s.length()) {
+            results.add(new ArrayList<>(path));
+            return ;
+        }
+
+        for (int i = startIndex; i < s.length(); i ++) {
+            if (isPalindrome(s, startIndex, i)) {
+                String str = s.substring(startIndex, i + 1);
+                path.add(str);
+                backtracking(s, i + 1);
+                path.removeLast();
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int start ,int end) {
+        for (int i = start, j = end; i < j; i ++, j --) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
