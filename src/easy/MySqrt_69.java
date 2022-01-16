a3a162ac20f65af8fdb247e14f6d0bc2ed9a8e8e
TtttttH <574173437@qq.com>
2022-01-16 22:04:08 +0000

    No.69(easy) sqrt(x),二分法，我是菜比

---
+++ src/easy/MySqrt_69.java
@@ -0,0 +1,25 @@
+package easy;
+
+/**
+ * No.69 Sqrt(x)
+ */
+public class MySqrt_69 {
+
+    public int mySqrt(int x) {
+        if (x == 1) {
+            return 1;
+        }
+        int left = 0;
+        int right = x;
+        while (right - left > 1) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
