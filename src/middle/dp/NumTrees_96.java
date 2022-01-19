27b554f30bc0d6323c02dbf7f41311569572e43d
TtttttH <574173437@qq.com>
2022-01-19 22:48:16 +0000

    以为是树题，结果是动归= =、

---
+++ src/middle/dp/NumTrees_96.java
@@ -0,0 +1,19 @@
+package middle.dp;
+
+/**
+ * No.96 不同的二叉搜索树
+ */
+public class NumTrees_96 {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i ++) {
+            for (int j = 1; j <= i; j ++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+
+        return dp[n];
+    }
+}
