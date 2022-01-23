dc2e6f1ba1410388c544a4ac97ea82341987a148
TtttttH <574173437@qq.com>
2022-01-23 01:16:33 +0000

    三重复数字位运算

--- src/middle/bitCompute/No_137.java
+++ src/middle/bitCompute/No_137.java
@@ -6,6 +6,12 @@ package middle.bitCompute;
 public class No_137 {
     //位运算
     public int singleNumber(int[] nums) {
-        
+        int a = 0, b = 0;
+         for (int i : nums) {
+             b = ~a & (b ^ i);
+             a = ~b & (a ^ i);
+         }
+
+         return b;
     }
 }
