6e8c89ac42aafb3c88c4227a0e6c6934f9294d8e
TtttttH <574173437@qq.com>
2022-02-03 07:10:00 +0000

    No.518 零钱兑换II 优化内循环逻辑

--- src/middle/dp/No_518.java
+++ src/middle/dp/No_518.java
@@ -8,11 +8,8 @@ public class No_518 {
         int[] dp = new int[amount + 1];
         dp[0] = 1;
         for (int coin : coins) {
-            for (int j = 1; j <= amount; j++) {
-                if (j >= coin) {
-                    dp[j] += dp[j - coin];
-                }
-
+            for (int j = coin; j <= amount; j++) {
+                dp[j] += dp[j - coin];
             }
         }
 
