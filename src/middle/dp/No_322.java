981fbd9041fe0f24766b0eb4f667f9701303e62f
TtttttH <574173437@qq.com>
2022-02-03 07:53:22 +0000

    No.322 零钱兑换 思路正确，但是没想出来最大值初始化,标记一下

---
+++ src/middle/dp/No_322.java
@@ -0,0 +1,23 @@
+package middle.dp;
+
+/**
+ * No.322 零钱兑换
+ */
+public class No_322 {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        for (int i = 0; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE; //注意这里初始化取最大值
+        }
+        dp[0] = 0;
+        for (int coin : coins) {
+            for (int j = coin; j <= amount; j ++) {
+                if (dp[j - coin] != Integer.MAX_VALUE) {
+                    dp[j] = Math.min(dp[j], dp[j - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
