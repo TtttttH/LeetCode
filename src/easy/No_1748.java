a0ee0fc8154553587bb0ff3fa11ba48fdb424542
TtttttH <574173437@qq.com>
2022-02-06 01:53:25 +0000

    每日一题，初六快乐

---
+++ src/easy/No_1748.java
@@ -0,0 +1,21 @@
+package easy;
+
+/**
+ * No.1748 唯一元素的和
+ */
+public class No_1748 {
+    public int sumOfUnique(int[] nums) {
+        int[] count = new int[101];
+        for (int num : nums) {
+            count[num] ++;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (count[i] == 1) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
