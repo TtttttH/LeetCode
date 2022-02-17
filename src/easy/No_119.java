d92df53e6fdcff946a579ea78334b1ce35e779a6
TtttttH <574173437@qq.com>
2022-02-17 15:55:25 +0000

    No.119 优化思路，使用滚动数组pre替换cur记录上一层list

--- src/easy/No_119.java
+++ src/easy/No_119.java
@@ -8,25 +8,36 @@ import java.util.List;
  */
 public class No_119 {
     public List<Integer> getRow(int rowindex) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        return getRow(rowindex, list);
+        List<Integer> pre = new ArrayList<>();
+        for (int i = 0; i <= rowindex; i ++) {
+            List<Integer> cur = new ArrayList<>();
+            for (int j = 0; j <= i; j ++) {
+                if (j == 0 || j == i) {
+                    cur.add(1);
+                } else {
+                    cur.add(pre.get(j - 1) + pre.get(j));
+                }
+            }
+
+            pre = cur;
+        }
+        return pre;
     }
 
-    private List<Integer> getRow(int rowIndex, List<Integer> row) {
-        if (rowIndex == 0) {
-            return row;
-        }
-
-        List<Integer> nextRow = new ArrayList<>();
-        nextRow.add(1);
-        for (int i = 0; i < row.size() - 1; i ++) {
-            nextRow.add(row.get(i) + row.get(i + 1));
-        }
-
-        nextRow.add(1);
-        return getRow(rowIndex - 1, nextRow);
-    }
+//    private List<Integer> getRow(int rowIndex, List<Integer> row) {
+//        if (rowIndex == 0) {
+//            return row;
+//        }
+//
+//        List<Integer> nextRow = new ArrayList<>();
+//        nextRow.add(1);
+//        for (int i = 0; i < row.size() - 1; i ++) {
+//            nextRow.add(row.get(i) + row.get(i + 1));
+//        }
+//
+//        nextRow.add(1);
+//        return getRow(rowIndex - 1, nextRow);
+//    }
 
 
     public static void main(String[] args) {
