470766cd793400774e2de204d19e0ae8c31757eb
TtttttH <574173437@qq.com>
2022-03-19 16:10:30 +0000

    No.606 根据二叉树创建字符串 迭代写法，难度明显增大

--- src/easy/BinaryTree/No_606.java
+++ src/easy/BinaryTree/No_606.java
@@ -1,24 +1,53 @@
 package easy.BinaryTree;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 /**
  * No.606 根据二叉树创建字符串
  */
 public class No_606 {
     public String tree2str(TreeNode root) {
+//        if (root == null) return "";
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(root.val);
+//        String left = tree2str(root.left);
+//        String right = tree2str(root.right);
+//        if (!left.equals("") && !right.equals("")) {
+//            sb.append('(').append(left).append(")(").append(right).append(')');
+//        } else if (left.equals("") && !right.equals("")) {
+//            sb.append("()(").append(right).append(')');
+//        } else if (!left.equals("")) {
+//            sb.append('(').append(left).append(')');
+//        }
+//
+//        return sb.toString();
         if (root == null) return "";
         StringBuilder sb = new StringBuilder();
-        sb.append(root.val);
-        String left = tree2str(root.left);
-        String right = tree2str(root.right);
-        if (!left.equals("") && !right.equals("")) {
-            sb.append('(').append(left).append(")(").append(right).append(')');
-        } else if (left.equals("") && !right.equals("")) {
-            sb.append("()(").append(right).append(')');
-        } else if (!left.equals("")) {
-            sb.append('(').append(left).append(')');
+        Deque<TreeNode> deque = new LinkedList<>();
+        Set<TreeNode> visited = new HashSet<>();
+        deque.push(root);
+        while (!deque.isEmpty()) {
+            TreeNode node = deque.pop();
+            if (visited.contains(node)) {
+                if (node != root) sb.append(')');
+            } else {
+                visited.add(node);
+                if (node != root) sb.append('(');
+                sb.append(root.val);
+                TreeNode left = node.left;
+                TreeNode right = node.right;
+                if (left == null && right != null) {
+                    sb.append("()");
+                }
+
+                if (right != null) {
+                    deque.push(right);
+                }
+
+                if (left != null) {
+                    deque.push(left);
+                }
+            }
         }
 
         return sb.toString();
